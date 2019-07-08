9fe512a96258512fa6bd36aa168c0d7b7cc90d57
S8001023456 <52515432+S8001023456@users.noreply.github.com>
2019-07-08 14:14:40 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,33 @@
 
 
 function start(e) {
-
+    var name=[
+{
+    char:'陳',
+    big5:'B3AF',
+    cns:'1-5D73',
+    unicode:'9673'    },
+    {char:'奕',
+    big5:'ABB3',
+    cns:'1-5055',
+    unicode:'5955'    },
+    {char:'江',
+    big5:'A6BF',
+    cns:'1-4840',
+    unicode:'6C5F'    }
+];
+    var el = document.getElementById("div1");
+    for (var i=0; i<3; i++)
+    {
+    el.innerHTML += name[i].char;
+    el.innerHTML +=' big5 = ';
+    el.innerHTML += name[i].big5;
+    el.innerHTML +=' cns = ';
+    el.innerHTML += name[i].cns;
+    el.innerHTML +=' unicode = ';
+    el.innerHTML += name[i].unicode;
+    el.innerHTML +='<br>';
+   };
 }
 
 /*
